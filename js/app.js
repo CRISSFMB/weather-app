29a7802678aaf80d700183ec8078872ee6d8769d
cristian mosquera <cmosqbone1@gmail.com>
2023-03-22 08:10:18 +0100

    finish proyect

--- js/app.js
+++ js/app.js
@@ -35,21 +35,40 @@ function getDataWeather(url) {
 function showResult(data) {
   console.log(data);
   const {
+    name,
     main: { temp, temp_max, temp_min },
   } = data;
 
   const tempNormal = kevinTodeDegrees(temp);
-  const tempMax = kevinTodeDegrees(temp_max);
-  const tempMin = kevinTodeDegrees(temp_min);
+  const tempMaximun = kevinTodeDegrees(temp_max);
+  const tempMinimun = kevinTodeDegrees(temp_min);
 
-  const paragraph = document.createElement("p");
-  paragraph.classList.add("font-bold", "text-6xl");
-  paragraph.innerHTML = `${tempNormal} &#8451`;
+  const cityName = document.createElement("p");
+  cityName.textContent = `weather in ${name}`;
+  cityName.classList.add("font-bold", "text-2xl");
+
+  const tempNormalParagraph = document.createElement("p");
+  tempNormalParagraph.classList.add("font-bold", "text-6xl");
+  tempNormalParagraph.innerHTML = `${tempNormal} &#8451`;
+
+  const tempMaxParagraph = document.createElement("p");
+  tempMaxParagraph.classList.add("text-xl");
+  tempMaxParagraph.innerHTML = `Max: ${tempMaximun} &#8451`;
+
+  const tempMinParagraph = document.createElement("p");
+  tempMinParagraph.classList.add("text-xl");
+  tempMinParagraph.innerHTML = `Min: ${tempMinimun} &#8451`;
 
   const div = document.createElement("div");
   div.classList.add("text-center", "text-white");
-  div.appendChild(paragraph);
+
+  div.appendChild(tempNormalParagraph);
+  div.appendChild(tempMaxParagraph);
+  div.appendChild(tempMinParagraph);
+  div.appendChild(cityName);
+
   const resultado = document.querySelector("#resultado");
+
   resultado.append(div);
 }
 
