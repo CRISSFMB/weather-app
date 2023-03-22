8a2dac3a0c175ecfa61026f11728e73b6c9e7e73
cristian mosquera <cmosqbone1@gmail.com>
2023-03-22 07:58:06 +0100

    adding fn kelvinToDegrees

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#formulary");
 const select = document.querySelector("#pais");
+const resultado = document.querySelector("#resultado");
 
 document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", getweather);
@@ -11,7 +12,7 @@ function getweather(e) {
   const city = document.querySelector("#city").value;
 
   if (city === "") {
-    console.log("the city cannot be empty");
+    showError("please write a city");
     return;
   }
 
@@ -23,7 +24,70 @@ function getweather(e) {
 
 function getDataWeather(url) {
   fetch(url)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((res) => {
+      cleanHTML();
+      return res.json();
+    })
+    .then((data) => showResult(data))
     .catch((e) => console.log(e));
+}
+
+function showResult(data) {
+  console.log(data);
+  const {
+    main: { temp, temp_max, temp_min },
+  } = data;
+
+  const tempNormal = kevinTodeDegrees(temp);
+  const tempMax = kevinTodeDegrees(temp_max);
+  const tempMin = kevinTodeDegrees(temp_min);
+
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("font-bold", "text-6xl");
+  paragraph.innerHTML = `${tempNormal} &#8451`;
+
+  const div = document.createElement("div");
+  div.classList.add("text-center", "text-white");
+  div.appendChild(paragraph);
+  const resultado = document.querySelector("#resultado");
+  resultado.append(div);
+}
+
+function kevinTodeDegrees(deg) {
+  return parseInt(deg - 273.15);
+}
+
+function showError(msg) {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = msg;
+  paragraph.classList.add(
+    "error",
+    "bg-red-100",
+    "border-red-400",
+    "text-red-700",
+    "px-4",
+    "py-3",
+    "rounded",
+    "max-w-md",
+    "mx-auto",
+    "mt-6",
+    "text-center"
+  );
+
+  const ExistError = document.querySelector(".error");
+
+  if (!ExistError) {
+    const container = document.querySelector(".container");
+    container.appendChild(paragraph);
+    setTimeout(() => {
+      paragraph.remove();
+    }, 3000);
+    return;
+  }
+}
+
+function cleanHTML() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
 }
